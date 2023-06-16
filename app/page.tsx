880287416f6343398ae3793d94a2f3c53cdbbb0c
Ernest Leung <ernestleung52@gmail.com>
2023-06-16 00:52:46 -0700

    Added Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<>
+		<div className=' bg-slate-800 text-slate-100 h-screen snap-y snap-mandatory overflow-scroll z-0'>
 			{/* Header */}
 			<Header />
 
 			{/* Hero */}
+			<section id='hero' className='snap-center'>
+				<Hero />
+			</section>
 
 			{/* About */}
-		</>
+		</div>
 	);
 }
