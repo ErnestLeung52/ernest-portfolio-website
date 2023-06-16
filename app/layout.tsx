d729c752d8efb15cb8b21350c7a0302d35b1fb58
Ernest Leung <ernestleung52@gmail.com>
2023-06-16 00:54:38 -0700

    moved all css stylings to page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
-			<body className={`${inter.className} bg-slate-800 text-slate-100`}>{children}</body>
+			<body className={`${inter.className}`}>{children}</body>
 		</html>
 	);
 }
