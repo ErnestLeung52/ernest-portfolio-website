f2226b74f512459d927f25804c1542177c446aa0
Ernest Leung <ernestleung52@gmail.com>
2023-06-20 14:17:32 -0700

    Added animation and styling to About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
+import About from '@/components/About';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Image from 'next/image';
 
 export default function Home() {
 	return (
+		// z-0 base layer
 		<div className=' bg-slate-800 text-slate-100 h-screen snap-y snap-mandatory overflow-scroll z-0'>
 			{/* Header */}
 			<Header />
 
 			{/* Hero */}
-			<section id='hero' className='snap-center'>
+			<section id='hero' className='snap-start'>
 				<Hero />
 			</section>
 
 			{/* About */}
+			<section id='about' className='snap-center'>
+				<About />
+			</section>
 		</div>
 	);
 }
