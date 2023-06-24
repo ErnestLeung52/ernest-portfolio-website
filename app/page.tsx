1995995deb14eea0b53cdee10f10d669cdf28c3b
Ernest Leung <ernestleung52@gmail.com>
2023-06-23 18:38:36 -0700

    Added scroll to top button

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
+import SmoothScrollLink from '@/components/SmoothScroll';
 import WorkExperience from '@/components/WorkExperience';
 import Image from 'next/image';
 
@@ -43,6 +44,18 @@ export default function Home() {
 			<section id='contact' className='snap-start'>
 				<ContactMe />
 			</section>
+
+			<SmoothScrollLink to='hero'>
+				<footer className='sticky bottom-5 w-full cursor-pointer'>
+					<div className='flex items-center justify-center'>
+						<img
+							className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+							src=''
+							alt='TOP'
+						/>
+					</div>
+				</footer>
+			</SmoothScrollLink>
 		</div>
 	);
 }
