87e288a5b3b5d7c3c2890fca2446308ee2d2cb3b
Ernest Leung <ernestleung52@gmail.com>
2023-06-26 23:02:59 -0700

    redesign work exp section

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -2,24 +2,44 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
+import { ITypeWorkData } from '@/types/interface';
 
-type Props = {};
+type Props = {
+	experience: ITypeWorkData;
+};
 
-const ExperienceCard = (props: Props) => {
+const ExperienceCard = ({ experience }: Props) => {
+	const { company, date, logo, role, bulletPoints }: ITypeWorkData = experience;
+
 	return (
-		<article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
-			<motion.img
-				initial={{ y: -100, opacity: 0 }}
-				whileInView={{ y: 0, opacity: 1 }}
-				transition={{ duration: 1.2 }}
-				viewport={{ once: true }}
-				className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
-				src='https://logowik.com/content/uploads/images/amazon6707.jpg'
-				alt=''
-			/>
+		<article className='flex flex-col rounded-lg space-y-7 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-slate-500 p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+			<div className='flex flex-row space-x-10 items-center justify-center'>
+				<div>
+					<motion.img
+						initial={{ y: -100, opacity: 0 }}
+						whileInView={{ y: 0, opacity: 1 }}
+						transition={{ duration: 1.2 }}
+						viewport={{ once: true }}
+						className='w-16 h-16 rounded-full md:w-24 md:h-24 object-cover object-center'
+						src='https://logowik.com/content/uploads/images/amazon6707.jpg'
+						alt=''
+					/>
+				</div>
+				<div>
+					<h4 className='text-3xl font-light'>{company}</h4>
+					<p className='font-bold text-xl mt-1'>{role}</p>
+					<p className='text-xl mt-1'>{date}</p>
+				</div>
+			</div>
 
-			<div className='px-0 md:px-10'>
-				<h4 className='text-4xl font-light'>Software Engineer</h4>
+			<ul className='list-disc space-y-2 ml-5 text-sm'>
+				{bulletPoints.map((point, i) => (
+					<li key={i}>{point}</li>
+				))}
+			</ul>
+
+			{/* <div className='px-0 md:px-10'>
+				<h4 className='text-4xl font-light'>{company}</h4>
 				<p className='font-bold text-2xl mt-1'>Amazon</p>
 				<div className='flex space-x-2 my-2'>
 					<img
@@ -46,7 +66,7 @@ const ExperienceCard = (props: Props) => {
 					<li>Summary</li>
 					<li>Summary</li>
 				</ul>
-			</div>
+			</div> */}
 		</article>
 	);
 };
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -3,10 +3,14 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import ExperienceCard from './ExperienceCard';
+import workExperienceData from '../data/workExperienceData.json';
+import { ITypeWorkData } from '@/types/interface';
 
 type Props = {};
 
 const WorkExperience = (props: Props) => {
+	const experienceData: ITypeWorkData[] = workExperienceData;
+
 	return (
 		<motion.div
 			initial={{ opacity: 0 }}
@@ -16,11 +20,10 @@ const WorkExperience = (props: Props) => {
 		>
 			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
 
-			<div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-gray-400'>
-				<ExperienceCard />
-				<ExperienceCard />
-				<ExperienceCard />
-				<ExperienceCard />
+			<div className='mt-10 w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-gray-400'>
+				{experienceData.map((experience, i) => (
+					<ExperienceCard key={i} experience={experience} />
+				))}
 			</div>
 		</motion.div>
 	);
