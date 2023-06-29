463d05078b83d4f45bd3f407bd352de9e66b833e
Ernest Leung <ernestleung52@gmail.com>
2023-06-28 17:04:18 -0700

    added resume download link and intro content

--- components/About.tsx
+++ components/About.tsx
@@ -2,10 +2,17 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
+import { DocumentArrowDownIcon } from '@heroicons/react/24/outline';
 
 type Props = {};
 
 const About = (props: Props) => {
+	const resumeURL = 'https://ernest-leung-resume.s3.us-west-1.amazonaws.com/Ernest_Leung_Resume.pdf';
+
+	const viewResumeHandler = () => {
+		window.open(resumeURL, '_blank', 'noopener,noreferrer');
+	};
+
 	return (
 		<motion.div
 			initial={{ opacity: 0 }}
@@ -26,21 +33,29 @@ const About = (props: Props) => {
 				}}
 				transition={{ duration: 1.2 }}
 				viewport={{ once: true }}
-				className='-mb-20 md:mb-0 flex shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
+				className='-mb-28 md:mb-0 flex shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
 				src='profile-pic.jpg'
 			/>
 
-			<div className='space-y-10 px-0 md:px-10'>
+			<div className='flex flex-col items-center space-y-10 px-0 md:px-10'>
 				<h4 className='text-4xl font-semibold'>
 					Here is a <span className='underline decoration-emerald-500/50'>little</span> background
 				</h4>
 				<p className='text-base'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
+					I'm Ernest Leung, a Full Stack Software Engineer specializing in{' '}
+					<span className='font-semibold text-emerald-500'> JavaScript, TypeScript, React, and Node.js </span>
+					. My experiences range from enhancing app performance to managing complex Node.js projects and
+					creating interactive React interfaces. I have a solid foundation in AWS serverless infrastructure,
+					coupled with a passion for problem-solving and lifelong learning. As a collaborative team player, I
+					strive to meet client needs and deliver successful project outcomes.
 				</p>
+				<div
+					className='group flex flex-row w-44 justify-center px-2 py-2 rounded-3xl bg-slate-600 hover:bg-emerald-500 items-center space-x-1 cursor-pointer'
+					onClick={viewResumeHandler}
+				>
+					<DocumentArrowDownIcon className='h-6 w-6 text-gray-400 group-hover:text-white' />
+					<div className='text-sm text-gray-400 font-semibold group-hover:text-white'>View Resume</div>
+				</div>
 			</div>
 		</motion.div>
 	);
