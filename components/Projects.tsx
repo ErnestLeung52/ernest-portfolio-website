70ae85121dd10a0cebee43a30354c7a6e7e04de1
Ernest Leung <ernestleung52@gmail.com>
2023-06-28 17:41:26 -0700

    fixed component size for small screen; fixed background z-index

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,6 +6,8 @@ import { motion } from 'framer-motion';
 import { Tab } from '@headlessui/react';
 import rawProjectsData from '../data/projectsData.json';
 import { ITypeProject } from '@/types/interface';
+import { SocialIcon } from 'react-social-icons';
+import { LinkIcon } from '@heroicons/react/24/solid';
 
 type Props = {};
 
@@ -21,19 +23,19 @@ const Projects = (props: Props) => {
 			initial={{ opacity: 0 }}
 			whileInView={{ opacity: 1 }}
 			transition={{ duration: 1.5 }}
-			className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'
+			className='h-screen relative flex overflow-hidden flex-col text-left max-w-full mx-auto items-center z-20 '
 		>
 			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
 
-			<div className=''>
+			<div className='mt-16 relative w-full flex'>
 				<section aria-labelledby='features-heading' className='mx-auto max-w-7xl py-32 sm:px-2 lg:px-8'>
 					<div className='mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0'>
-						<div className='max-w-3xl'></div>
+						{/* <div className='max-w-3xl'></div> */}
 
 						<Tab.Group as='div' className='mt-4'>
-							<div className='-mx-4 flex overflow-x-auto sm:mx-0'>
+							<div className='-mx-4 flex  sm:mx-0'>
 								<div className='flex-auto border-b border-gray-400 mx-4 sm:px-0'>
-									<Tab.List className='-mb-px flex space-x-10'>
+									<Tab.List className='-mb-px flex space-x-5'>
 										{projectsData.map((project) => (
 											<Tab
 												key={project.name}
@@ -55,20 +57,48 @@ const Projects = (props: Props) => {
 
 							<Tab.Panels as={Fragment}>
 								{projectsData.map((project) => (
-									<Tab.Panel key={project.name} className='space-y-16 pt-10 lg:pt-16'>
+									<Tab.Panel key={project.name} className='relative pt-10 px-4 lg:pt-16 z-20'>
 										{project.features.map((feature) => (
 											<div
 												key={feature.name}
 												className='flex flex-col-reverse lg:grid lg:grid-cols-12 lg:gap-x-8'
 											>
 												<div className='mt-6 lg:col-span-5 lg:mt-0'>
-													<h3 className='text-lg font-medium text-gray-400'>
-														{feature.name}
-													</h3>
-													<p className='mt-2 text-sm text-gray-400'>{feature.description}</p>
+													<div className='flex flex-row items-center space-x-3'>
+														<h3 className='text-lg font-medium text-gray-400 lg:text-2xl'>
+															{feature.name}
+														</h3>
+														<div className='flex flex-row items-center cursor-pointer'>
+															<SocialIcon
+																url={feature.githubURL}
+																fgColor='gray'
+																bgColor='transparent'
+																style={{ height: 40, width: 40 }}
+																target='_blank'
+																rel='noopener noreferrer'
+															/>
+															{feature.hostURL && (
+																<a
+																	href={feature.hostURL}
+																	target='_blank'
+																	rel='noopener noreferrer'
+																>
+																	<LinkIcon className='w-5 h-5 text-gray-400' />
+																</a>
+															)}
+														</div>
+													</div>
+													<div className='mt-2 text-sm text-gray-400 lg:text-base'>
+														{feature.description.map((paragraph, i) => (
+															<p key={i} className='my-2'>
+																{paragraph}
+															</p>
+														))}
+													</div>
 												</div>
 												<div className='lg:col-span-7'>
-													<div className='aspect-h-1 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:aspect-h-2 sm:aspect-w-5'>
+													{/* <div className='aspect-h-1 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:aspect-h-2 sm:aspect-w-5'> */}
+													<div className='w-[300px] mx-auto overflow-hidden rounded-lg sm:w-[420px] lg:w-[500px]'>
 														<img
 															src={feature.imageSrc}
 															alt={feature.imageAlt}
@@ -85,8 +115,7 @@ const Projects = (props: Props) => {
 					</div>
 				</section>
 			</div>
-
-			<div className='w-full absolute top-[30%] bg-emerald-700/10 left-0 h-[500px] -skew-y-12' />
+			<div className='w-full absolute top-[30%] bg-emerald-800/10 left-0 h-[500px] -skew-y-12 z-0' />
 		</motion.div>
 	);
 };
