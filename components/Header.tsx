fa1490cd6e7ef3a82ce90cca28f9c583b35c0cb8
Ernest Leung <ernestleung52@gmail.com>
2023-06-28 17:35:40 -0700

    fixed contact button to navigate to contact section

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,14 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import { motion } from 'framer-motion';
-// import ThemeToggleButton from './ThemeToggleButton';
+import SmoothScrollLink from './SmoothScroll';
+import { EnvelopeIcon } from '@heroicons/react/24/outline';
 
 type Props = {};
 
 export default function Header({}: Props) {
 	return (
-		<header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+		<header className='sticky top-0 p-5 flex justify-between max-w-7xl mx-auto z-20 items-center'>
 			<motion.div
 				initial={{ x: -500, opacity: 0, scale: 0.5 }}
 				animate={{ x: 0, opacity: 1, scale: 1 }}
@@ -17,21 +18,34 @@ export default function Header({}: Props) {
 				className='flex flex-row items-center'
 			>
 				{/* Social Icons */}
-				<SocialIcon url='https://github.com/ErnestLeung52' fgColor='gray' bgColor='transparent' />
-				<SocialIcon url='https://www.linkedin.com/in/ernestleung52/' fgColor='gray' bgColor='transparent' />
+				<SocialIcon
+					url='https://github.com/ErnestLeung52'
+					fgColor='gray'
+					bgColor='transparent'
+					target='_blank'
+					rel='noopener noreferrer'
+				/>
+				<SocialIcon
+					url='https://www.linkedin.com/in/ernestleung52/'
+					fgColor='gray'
+					bgColor='transparent'
+					target='_blank'
+					rel='noopener noreferrer'
+				/>
 			</motion.div>
 
 			<div className='flex flex-row items-center'>
 				{/* <ThemeToggleButton /> */}
-
 				<motion.div
 					initial={{ x: 500, opacity: 0, scale: 0.5 }}
 					animate={{ x: 0, opacity: 1, scale: 1 }}
 					transition={{ duration: 1.5 }}
 					className='flex flex-row items-center text-gray-300 cursor-pointer'
 				>
-					<SocialIcon className='cursor-pointer' network='email' fgColor='gray' bgColor='transparent' />
-					<p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+					<SmoothScrollLink to='#contact'>
+						<EnvelopeIcon className='h-7 w-7 inline mr-2 text-gray-400' />
+						<p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+					</SmoothScrollLink>
 				</motion.div>
 			</div>
 		</header>
