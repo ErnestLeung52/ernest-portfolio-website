1d6c58b8f797a34ce7f6ebce8e2bb9ee7e84f10a
Ernest Leung <ernestleung52@gmail.com>
2023-07-14 16:08:04 -0700

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang='en'>
 			<body className={`${inter.className}`}>
 				<ThemeProvider attribute='class'>{children}</ThemeProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
